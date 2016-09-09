a3e79fc23ec178e1aa74af026cc31341118bf163
efrain_ventura <efra1323@gmail.com>
2016-09-09 01:01:17 -0500

    funciones compras

--- app/Http/Controllers/ComprasController.php
+++ app/Http/Controllers/ComprasController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 use App\Http\Requests;
 use App\Http\Controllers;
@@ -19,20 +20,36 @@ class ComprasController extends Controller
     }
 
 
-     public function destroy()
+     public function destroy(Request $request)
     {
-    	return "Borrar";
+    	$data= (object) $request->json()->all();
+
+        $delete = $data->id;
+
+        DB::table('compras')->where('id', '=', $delete)->delete();
     }
 
 
-     public function update()
+     public function update(Request $request)
     {
-    	return "Actualizar";
+    	$data= (object) $request->json()->all();
+
+        DB::table('compras')
+            ->where('id', $data->id)
+            ->update(['fecha'=>$data->fecha,'total'=>$data->total]);
+
+        return response()->json($data);
     }
     
 
-     public function store()
+     public function store(Request $request)
     {
-    	return "Guardar";
+    	
+        $data = (object) $request->json()->all();
+
+        $id = DB::table('compras')->insertGetId(['total' => $data->total, 'fecha' => Carbon::now()]);
+
+        $data->id=$id;
+        return response()->json($data);
     }
 }
--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -10,27 +10,35 @@ use App\Http\Controllers;
 
 class ProductosController extends Controller
 {
-    //index
+    
     public function index()
     {
+
     	return response()->json(DB::table('productos')->get());
+
     }
 
 
      public function destroy()
     {
+
     	return "Borrar";
+
     }
 
 
      public function update()
     {
+
     	return "Actualizar";
+
     }
     
 
      public function store()
     {
-    	return "Guardar";
+
+    	return $id = DB::table('productos')->insertGetId(['nombre' => '', 'stock' => '','precio'=>0]);
+
     }
 }
